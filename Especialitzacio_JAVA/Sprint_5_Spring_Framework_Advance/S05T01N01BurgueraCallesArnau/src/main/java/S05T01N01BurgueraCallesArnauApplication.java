12b9c8c55f0f2e3f971bd7da769a7cd3848ca4e0
ArnauBurguera <aburguera.ab@gmail.com>
2023-10-09 21:18:06 +0200

    Added sprint 5

---
+++ Especialitzacio_JAVA/Sprint_5_Spring_Framework_Advance/S05T01N01BurgueraCallesArnau/src/main/java/S05T01N01BurgueraCallesArnauApplication.java
@@ -0,0 +1,13 @@
+package catitacademy_barcelonactiva_BurgueraCallesArnau.s05_t01_n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01N01BurgueraCallesArnauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01N01BurgueraCallesArnauApplication.class, args);
+	}
+
+}
---
+++ Especialitzacio_JAVA/Sprint_5_Spring_Framework_Advance/S05T01N01BurgueraCallesArnau/src/test/java/S05T01N01BurgueraCallesArnauApplicationTests.java
@@ -0,0 +1,13 @@
+package catitacademy_barcelonactiva_BurgueraCallesArnau.s05_t01_n01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N01BurgueraCallesArnauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
