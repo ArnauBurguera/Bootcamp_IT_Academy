a89475a2061c6213e8b72f6ed04a915d1d8f2050
ArnauBurguera <aburguera.ab@gmail.com>
2023-10-10 09:21:23 +0200

    Nou sprint 5 tasca 1 nivell 1, filepath mes curt

--- Especialitzacio_JAVA/Sprint_5_Spring_Framework_Advance/s05t01n01/src/test/java/BurgueraCallesArnau/s05t01n01/S05t01n01ApplicationTests.java
+++ Especialitzacio_JAVA/Sprint_5_Spring_Framework_Advance/s05t01n01/src/test/java/BurgueraCallesArnau/s05t01n01/S05t01n01ApplicationTests.java
@@ -1,10 +1,10 @@
-package catitacademy_barcelonactiva_BurgueraCallesArnau.s05_t01_n01;
+package BurgueraCallesArnau.s05t01n01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class S05T01N01BurgueraCallesArnauApplicationTests {
+class S05t01n01ApplicationTests {
 
 	@Test
 	void contextLoads() {
