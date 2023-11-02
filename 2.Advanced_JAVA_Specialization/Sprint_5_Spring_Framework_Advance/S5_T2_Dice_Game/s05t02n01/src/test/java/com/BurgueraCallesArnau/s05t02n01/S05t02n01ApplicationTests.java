b1161547dc7c95314f5c7050cde01ea191f5d914
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-02 11:06:55 +0100

    project created

---
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/test/java/com/BurgueraCallesArnau/s05t02n01/S05t02n01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BurgueraCallesArnau.s05t02n01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05t02n01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
