1d98d0987e6ef101ca99c0bf79043ded6fa5e193
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-02 12:02:00 +0100

    added Game class

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/model/domain/Game.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/model/domain/Game.java
@@ -3,22 +3,17 @@ package com.BurgueraCallesArnau.s05t02n01.model.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Date;
-import java.util.List;
-
 @Entity
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class Player {
+public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
-    private Date registrationDate;
-
-    @OneToMany(mappedBy = "player")
-    private List<Game> games;
+    private int dice1;
+    private int dice2;
+    private boolean won;
 }
