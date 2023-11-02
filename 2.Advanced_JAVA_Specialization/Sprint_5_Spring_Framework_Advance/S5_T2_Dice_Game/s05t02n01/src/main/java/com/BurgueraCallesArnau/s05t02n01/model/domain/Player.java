83f0568c515a4b3307573902d97583ce7bac24ff
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-02 11:40:35 +0100

    Added structure and player class

---
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/model/domain/Player.java
@@ -0,0 +1,23 @@
+package com.BurgueraCallesArnau.s05t02n01.model.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Date registrationDate;
+}
