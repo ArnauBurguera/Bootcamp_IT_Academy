ce56dddb937bd166f57326021fab0b2b9afbeafc
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-05 13:59:51 +0100

    Working but self reference infinite loop

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/controllers/rest/GameController.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/controllers/rest/GameController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/players/{playerid}/games")
+@RequestMapping("/players/{playerId}/games")
 public class GameController {
 
     @Autowired
