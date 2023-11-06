927f4388d2d5ffd0965f3b7230b9a02ed51b8a82
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-06 10:18:01 +0100

    Swagger annotations added for GAmeController

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/controllers/rest/GameController.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/controllers/rest/GameController.java
@@ -2,6 +2,8 @@ package com.BurgueraCallesArnau.s05t02n01.controllers.rest;
 
 import com.BurgueraCallesArnau.s05t02n01.model.domain.Game;
 import com.BurgueraCallesArnau.s05t02n01.service.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,20 +21,23 @@ public class GameController {
     private GameService gameService;
 
     @PostMapping("/play")
-    public ResponseEntity<Game> playGame(@PathVariable int playerId) {
+    @Operation(summary = "Play a game for a specific player", description = "This API allows you to create a new games.")
+    public ResponseEntity<Game> playGame(@Parameter(description = "The player id", required = true) @PathVariable int playerId) {
         Game game = gameService.playGame(playerId);
         return ResponseEntity.status(HttpStatus.CREATED).body(game);
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<String> deleteGamesForPlayer(@PathVariable int playerId) {
+    @Operation(summary = "Delete all game records for a specific player", description = "This API allows you to delete game record for a given player")
+    public ResponseEntity<String> deleteGamesForPlayer(@Parameter(description = "The player id", required = true)@PathVariable int playerId) {
         gameService.deleteGamesForPlayer(playerId);
 
         return ResponseEntity.ok("All games for the player have been deleted.");
     }
 
     @GetMapping("/list")
-    public ResponseEntity<List<Game>> getPlayerGames(@PathVariable int playerId) {
+    @Operation(summary = "List game records for a specific player", description = "This API allows you to list game record for a given player")
+    public ResponseEntity<List<Game>> getPlayerGames(@Parameter(description = "The player id", required = true)@PathVariable int playerId) {
         List<Game> games = gameService.getGamesForPlayer(playerId);
         return ResponseEntity.ok(games);
     }
