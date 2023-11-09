7f5f7403bb62cc090a1d69112adc4af014ab3317
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-09 10:36:55 +0100

    Added secret key to application properties

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/security/JwtService.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/security/JwtService.java
@@ -2,10 +2,17 @@ package com.BurgueraCallesArnau.s05t02n01.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+    @Autowired
+    private static Environment environment;
+
+    @NonNull private static final String SECRET_KEY = environment.getProperty("secret.key");
     public String extractUserName(String token) {
         return null;
     }
