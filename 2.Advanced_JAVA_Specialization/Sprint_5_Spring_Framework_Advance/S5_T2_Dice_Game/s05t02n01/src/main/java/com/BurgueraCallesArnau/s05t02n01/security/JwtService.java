c1402048c68461d755a74342e90b58733592ba50
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-09 10:55:20 +0100

    Alarna sounding

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/security/JwtService.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/security/JwtService.java
@@ -7,9 +7,12 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -19,12 +22,25 @@ public class JwtService {
 
     @NonNull private static final String SECRET_KEY = environment.getProperty("secret.key");
     public String extractUserName(String token) {
-        return null;
+        return extractClaim(token,Claims::getSubject);
     }
 
     public  <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey()), Signa
     }
 
     private Claims extractAllClaims(String token){
