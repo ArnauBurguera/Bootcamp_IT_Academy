8026eba58031f457d6a54a45e25de322970499f2
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-09 09:51:35 +0100

    Added JwtAuthenticationFilter

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/repository/PlayerRepository.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n01/src/main/java/com/BurgueraCallesArnau/s05t02n01/repository/PlayerRepository.java
@@ -4,7 +4,9 @@ import com.BurgueraCallesArnau.s05t02n01.model.domain.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<Player,Integer> {
-
+    Optional<Player> findByEmail(String email);
 }
