62bf4dd3348810350c844fb5a996913c12e909b1
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-19 19:02:48 +0100

    Repo game updated

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/main/java/com/BurgueraCallesArnau/s05t02n01/repository/GameRepository.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/main/java/com/BurgueraCallesArnau/s05t02n01/repository/GameRepository.java
@@ -2,10 +2,10 @@ package com.BurgueraCallesArnau.s05t02n01.repository;
 
 import com.BurgueraCallesArnau.s05t02n01.model.domain.Game;
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends MongoRepository<Game, ObjectId> {
+public interface GameRepository extends JpaRepository<Game, ObjectId> {
 
 }
