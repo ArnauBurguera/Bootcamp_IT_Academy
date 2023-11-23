b1d14d2611dd0b3a953c04f6d4776d5a4eddf764
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-23 10:44:25 +0100

    SaveTest almost finished

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/repository/GameRepositoryTest.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/repository/GameRepositoryTest.java
@@ -1,9 +1,31 @@
 package com.BurgueraCallesArnau.s05t02n01.repository;
 
+import com.BurgueraCallesArnau.s05t02n01.model.domain.Game;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.junit.Assert.assertThat;
 
 @DataJpaTest
 public class GameRepositoryTest {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Test
+    public void gameRepository_SaveTest(){
+        Game game = Game.builder()
+                .dice1(3)
+                .dice2(4)
+                .won(true)
+                .playerId(new ObjectId("655c7adf06e4ae59f47979ca"))
+                .build();
+
+        Game savedGame = gameRepository.save(game);
+        /*Assertions.assertThat(savedGame).is*/
+    }
 
 
 }
