67fb11949e4be85e6074543559ee10438979039b
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-27 10:36:54 +0100

    First test working

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/repository/GameRepositoryTest.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/repository/GameRepositoryTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Assertions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class GameRepositoryTest {
 
     @Autowired
