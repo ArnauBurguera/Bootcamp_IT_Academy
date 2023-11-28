179c740df300d942e9636e9eb3e2bcac0dca9528
ArnauBurguera <aburguera.ab@gmail.com>
2023-11-28 11:02:50 +0100

    Added DeleteAllGames test

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/repository/GameRepositoryTest.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/repository/GameRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.BurgueraCallesArnau.s05t02n01.repository;
 
 import com.BurgueraCallesArnau.s05t02n01.model.domain.Game;
+import com.BurgueraCallesArnau.s05t02n01.model.domain.Player;
 import org.bson.types.ObjectId;
 import org.assertj.core.api.Assertions;
 
@@ -9,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -17,9 +20,11 @@ public class GameRepositoryTest {
 
     @Autowired
     private GameRepository gameRepository;
+    @Autowired
+    private PlayerRepository playerRepository;
 
     @Test
-    public void gameRepository_SaveTest_ShouldReturnSavedGame(){
+    public void saveTest_ShouldReturnSavedGame(){
         Game game = Game.builder()
                 .dice1(3)
                 .dice2(4)
@@ -33,7 +38,7 @@ public class GameRepositoryTest {
     }
 
     @Test
-    public void gameRepository_DeleteAllTest_ShouldDeleteAllGamesInRepository(){
+    public void deleteAllTest_ShouldDeleteAllGamesInRepository(){
         Game game = Game.builder()
                 .dice1(3)
                 .dice2(4)
@@ -41,20 +46,49 @@ public class GameRepositoryTest {
                 .playerId(new ObjectId("655c7adf06e4ae59f47979ca"))
                 .build();
         Game game2 = Game.builder()
-                .dice1(5)
-                .dice2(6)
-                .won(false)
-                .playerId(new ObjectId("655c7adf06e4ae59f47612ca"))
                 .build();
 
         gameRepository.save(game);
         gameRepository.save(game2);
 
-        List<Game> gameList = gameRepository.findAll();
-        Assertions.assertThat(gameList).isNotNull();
-        Assertions.assertThat(gameList.size()).isEqualTo(2);
+        List<Game> gamesToDelete  = new ArrayList<>();
+        gamesToDelete .add(game);
+        gamesToDelete .add(game2);
+
+        gameRepository.deleteAll(gamesToDelete );
+        Assertions.assertThat(gameRepository.findAll()).isEmpty();
+    }
+
+    @Test
+    public void findByPlayerIdTest_ShouldReturnListOfGamesBasedOnPlayerId(){
+        Game game1 = Game.builder()
+                .build();
+        Game game2 = Game.builder()
+                .build();
+        Game game3 = Game.builder()
+                .build();
+
+        gameRepository.save(game1);
+        gameRepository.save(game2);
+        gameRepository.save(game3);
+
+        Player player = Player.builder()
+                .id(new ObjectId("655c7adf06e4ae59f47979ca"))
+                .build();
+
+        player.addGame(game1);
+        player.addGame(game2);
+        player.addGame(game3);
+        playerRepository.save(player);
+
+        gameRepository.findByPlayerId(new ObjectId("655c7adf06e4ae59f47979ca"));
+
+
+
 
     }
+
+
 
 
 }
