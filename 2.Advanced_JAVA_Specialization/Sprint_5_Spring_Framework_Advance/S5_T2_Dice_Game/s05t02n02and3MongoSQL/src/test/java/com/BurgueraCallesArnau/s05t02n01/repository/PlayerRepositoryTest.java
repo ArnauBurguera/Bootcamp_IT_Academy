194280abb0e9334e2aa1a672211fd1c2973f590b
Arnau Burguera <aburguera.ab@gmail.com>
2023-12-01 16:48:49 +0100

    Mongodb test with testcontainers and docker working beautifully

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/repository/PlayerRepositoryTest.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/repository/PlayerRepositoryTest.java
@@ -1,26 +1,45 @@
 package com.BurgueraCallesArnau.s05t02n01.repository;
 
-import com.BurgueraCallesArnau.s05t02n01.model.domain.Game;
 import com.BurgueraCallesArnau.s05t02n01.model.domain.Player;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
 
 import java.util.List;
 
 @DataMongoTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = {
+        "spring.test.database.replace=none",
+        "spring.data.mongodb.uri=mongodb://${mongodb.host}:${mongodb.port}/testdb"
+})
 public class PlayerRepositoryTest {
-    //TODO: findAll, delete, findByEmail(String email), findById(playerId)
+    //TODO: delete, findByEmail(String email), findById(playerId)
+    //"mongo:latest" in next line downloads latest Docker image for Mongo
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
 
     private final PlayerRepository playerRepository;//Field injection is not best practice
 
     @Autowired
     public PlayerRepositoryTest(PlayerRepository playerRepository) {
         this.playerRepository = playerRepository;
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        mongoDBContainer.start();
+        System.setProperty("mongodb.host", mongoDBContainer.getHost());
+        System.setProperty("mongodb.port", String.valueOf(mongoDBContainer.getMappedPort(27017)));
+    }
+
+    @BeforeEach
+    void setUp(){
+        playerRepository.deleteAll();
     }
 
     @DisplayName("Player Repository Save - Should return a Saved Player")
