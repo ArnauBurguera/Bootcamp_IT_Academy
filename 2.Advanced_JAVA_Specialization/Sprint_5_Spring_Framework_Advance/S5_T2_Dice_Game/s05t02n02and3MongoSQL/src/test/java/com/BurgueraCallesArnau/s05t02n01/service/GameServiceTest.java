01fad6ceb458696fe6857db85a91dc13f584bfd2
Arnau Burguera <aburguera.ab@gmail.com>
2023-12-02 12:44:54 +0100

    Added Mockito annotations on GAmeServiceTest

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/service/GameServiceTest.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/service/GameServiceTest.java
@@ -1,4 +1,22 @@
 package com.BurgueraCallesArnau.s05t02n01.service;
 
+import com.BurgueraCallesArnau.s05t02n01.repository.GameRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class GameServiceTest {
+    //TODO
+
+    @Mock
+    private GameRepository gameRepository;
+
+    @InjectMocks
+    private GameService gameService;
+
+    /*@Test
+    public void*/
 }
