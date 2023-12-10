99c45733e76f390af26e27d6a7919a5b6073b2f5
Arnau Burguera <aburguera.ab@gmail.com>
2023-12-10 10:04:17 +0100

    This is how things are

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/controllers/PlayerControllerTest.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/controllers/PlayerControllerTest.java
@@ -77,23 +77,6 @@ public class PlayerControllerTest {
         this.objectMapper = objectMapper;
     }
 
-    /*@Test
-    @DisplayName("Update Player Name - Should return 200 OK with updated player")
-    public void updatePlayerName_ShouldReturnOkWithUpdatedPlayer() throws Exception {
-        ObjectId playerId = new ObjectId("655c7adf06e4ae59f47979ca");
-        String newName = "NewName";
-        Player updatedPlayer = Player.builder().id(playerId).name(newName).build();
-
-        given(playerService.updatePlayerName(playerId, newName)).willReturn(updatedPlayer);
-
-        mockMvc.perform(put("/players/update/{id}", playerId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(newName))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(playerId.toHexString())))
-                .andExpect(jsonPath("$.name", is(newName)));
-    }*/
-
     @Test
     @DisplayName("Update Player Name - Should return 200 OK with updated player")
     public void updatePlayerName_ShouldReturnOkWithUpdatedPlayer1() throws Exception {
@@ -102,57 +85,17 @@ public class PlayerControllerTest {
                 .id(new ObjectId("655c7adf06e4ae59f47979ca"))
                 .name(newName)
                 .build();
-        /*when(playerService.updatePlayerName(updatedPlayer.getId(), newName)).thenReturn(updatedPlayer);*/
         given(playerService.updatePlayerName(player.getId(), newName)).willReturn(player);
 
-        MockHttpServletResponse result = mockMvc.perform(put("/players/update/{id}", player.getId())
+        mockMvc.perform(put("/players/update/{id}", player.getId())
                         .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.TEXT_PLAIN)//indiferent si es plaintext o json
-                        .content(/*objectMapper.writeValueAsString(*/newName)/*)*/) //SI ES OBJECTE RETORNA
+                        .contentType(MediaType.TEXT_PLAIN_VALUE)//indiferent si es plaintext o json
+                        .content(/*objectMapper.writeValueAsString(*/newName/*)*/) //SI ES OBJECTE RETORNA
+                        .characterEncoding("utf-8"))
                         .andDo(print())
 
-
-                        /*.content(new ObjectMapper().writeValueAsString(newName))*/
-
-               /* .andExpect(status().isOk())*/
-                .andExpect(jsonPath("$.name").value(player.getName()))
-                .andReturn().getResponse();
-
-        System.out.println("RESULT!!!!!: " + result.getContentAsString());
-                /*.andExpect(jsonPath("$.id").value(updatedPlayer.getId().toString()));*/
-    }
-
-    @Test
-    @DisplayName("Update Player Name - Should return 200 OK with updated player")
-    public void updatePlayerName_ShouldReturnOkWithUpdatedPlayer() throws Exception {
-        String newName = "newName";
-        /*String jsonContent = objectMapper.writeValueAsString(Map.of("name", newName));*/
-        Player updatedPlayer = Player.builder()
-                .id(new ObjectId("655c7adf06e4ae59f47979ca"))
-                .name(newName)
-                .build();
-
-       /* System.out.println("id ies " + updatedPlayer.toString());*/
-
-        given(playerService.updatePlayerName(updatedPlayer.getId(), newName)).willReturn(updatedPlayer);
-
-        /*MvcResult result = */mockMvc.perform(put("/players/update/{id}", updatedPlayer.getId())
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                       /* .content(new ObjectMapper().writeValueAsString(newName))) //200 but dosn't return*/
-                        .content(newName))//500 but returns
-                /*.andReturn();*/
-
-
-                .andExpect(status().isOk())
-                .andReturn();
-                /*.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id").value(updatedPlayer.getId()))
-                .andExpect(jsonPath("$.name").value(updatedPlayer.getName()));*/
-       /* MockHttpServletResponse response = result.getResponse();
-        System.out.println("Response Headers: " + response.getHeaderNames());
-        System.out.println("Response Content Type: " + response.getContentType());
-        System.out.println("Response Body: " + response.getContentAsString());*/
+                /*.andExpect(status().isOk())*/
+                .andExpect(jsonPath("$.name").value(player.getName()));
     }
 
     @Test
