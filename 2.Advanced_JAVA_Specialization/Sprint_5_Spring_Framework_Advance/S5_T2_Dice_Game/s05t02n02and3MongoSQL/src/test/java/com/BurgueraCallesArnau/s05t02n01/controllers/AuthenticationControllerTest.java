169a6c886d97e74cf180dda84b68c3a2885e0bbd
Arnau Burguera <aburguera.ab@gmail.com>
2023-12-10 19:56:17 +0100

    Deleted unnecessary imports

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/controllers/AuthenticationControllerTest.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/controllers/AuthenticationControllerTest.java
@@ -15,11 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,18 +25,15 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.mockito.Mockito.*;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/*@WebMvcTest(controllers = AuthenticationControllerTest.class)
-@AutoConfigureMockMvc(addFilters = false)//circumvent security*/
 @ExtendWith(MockitoExtension.class)
 public class AuthenticationControllerTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private /*final*/ MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Mock
     private AuthenticationService authenticationService;
@@ -51,11 +44,6 @@ public class AuthenticationControllerTest {
 
     @InjectMocks
     private AuthenticationController authenticationController;
-
-    /*@Autowired
-    AuthenticationControllerTest(MockMvc mockMvc) {
-        this.mockMvc = mockMvc;;
-    }*/
 
     @BeforeEach
     void setUp() {
