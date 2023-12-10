97f3100c7b8681b712cc619bd7389ffb72ffdbce
Arnau Burguera <aburguera.ab@gmail.com>
2023-12-10 12:21:03 +0100

    Improved update method test

--- 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/controllers/PlayerControllerTest.java
+++ 2.Advanced_JAVA_Specialization/Sprint_5_Spring_Framework_Advance/S5_T2_Dice_Game/s05t02n02and3MongoSQL/src/test/java/com/BurgueraCallesArnau/s05t02n01/controllers/PlayerControllerTest.java
@@ -2,6 +2,7 @@ package com.BurgueraCallesArnau.s05t02n01.controllers;
 
 import com.BurgueraCallesArnau.s05t02n01.controllers.rest.GameController;
 import com.BurgueraCallesArnau.s05t02n01.controllers.rest.PlayerController;
+import com.BurgueraCallesArnau.s05t02n01.model.domain.Game;
 import com.BurgueraCallesArnau.s05t02n01.model.domain.Player;
 import com.BurgueraCallesArnau.s05t02n01.model.domain.Role;
 import com.BurgueraCallesArnau.s05t02n01.security.JwtService;
@@ -11,8 +12,7 @@ import com.BurgueraCallesArnau.s05t02n01.service.PlayerService;
 import com.BurgueraCallesArnau.s05t02n01.utils.Constants;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bson.types.ObjectId;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -40,6 +40,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.assertj.core.api.Assertions;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
@@ -50,6 +52,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -72,33 +75,55 @@ public class PlayerControllerTest {
     @InjectMocks
     private PlayerController playerController;
 
+    private Player player;
+    private Player player2;
+    private final ObjectId PLAYERID;
+    private List<Game> games;
+
     @Autowired
     PlayerControllerTest(MockMvc mockMvc, ObjectMapper objectMapper) {
         this.mockMvc = mockMvc;
         this.objectMapper = objectMapper;
+        this.PLAYERID = new ObjectId("655c7adf06e4ae59f47979ca");
+    }
+
+    @BeforeEach
+    public void setUp(){
+
+        player = Player.builder()
+                .id(PLAYERID)
+                .name("Alex")
+                .role(Role.USER)//Has problems if not assigned
+                .build();
+        player2 = Player.builder()
+                .id(new ObjectId("655c7adf06e4ae59f47979cb"))
+                .name("Alexa")
+                .role(Role.USER)//Has problems if not assigned
+                .build();
+        games = Arrays.asList(
+                Game.builder().playerId(PLAYERID).won(true).build(),
+                Game.builder().playerId(PLAYERID).won(true).build(),
+                Game.builder().playerId(PLAYERID).won(false).build()
+        );
     }
 
     @Test
     @DisplayName("Update Player Name - Should return 200 OK with updated player")
     public void updatePlayerName_ShouldReturnOkWithUpdatedPlayer1() throws Exception {
         String newName = "newName";
-        Player player = Player.builder()
-                .id(new ObjectId("655c7adf06e4ae59f47979ca"))
-                .name(newName)
-                .role(Role.USER)
-                .build();
-        given(playerService.updatePlayerName(player.getId(), newName)).willReturn(player);
+        Player updatedplayer = this.player;
+        updatedplayer.setName(newName);
+        given(playerService.updatePlayerName(PLAYERID, newName)).willReturn(updatedplayer);
 
-        mockMvc.perform(put("/players/update/{id}", player.getId(), newName)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)//indiferent si es plaintext o json
-                        .content(/*objectMapper.writeValueAsString(*/newName/*)*/) //SI ES OBJECTE RETORNA
+        mockMvc.perform(put("/players/update/{id}", PLAYERID, newName)
+                        /*.accept(MediaType.APPLICATION_JSON)*/
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newName)
                         .characterEncoding("utf-8"))
-                       /* .param("name", newName))*/
                         .andDo(print())
 
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value(player.getName()));
+                .andExpect(jsonPath("$.name").value(updatedplayer.getName()));
     }
 
     @Test
@@ -113,5 +138,42 @@ public class PlayerControllerTest {
                 .andExpect(content().string(Constants.deletePlayerResponseBody));
 
         verify(playerService, times(1)).deletePlayer(playerId);
+    }
+
+    @Test
+    @DisplayName("Get All Players - Should return 200 OK with a list of players")
+    public void getAllPlayers_ShouldReturnOkWithListOfPlayers() throws Exception {
+        List<Player> players = Arrays.asList(this.player, this.player2);
+        given(playerService.getAllPlayers()).willReturn(players);
+
+        mockMvc.perform(get("/players/getAll")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(players.size()))
+                .andExpect(jsonPath("$[0].name").value(players.get(0).getName()))
+                .andExpect(jsonPath("$[0].role").value(players.get(0).getRole().toString()))
+                .andExpect(jsonPath("$[1].name").value(players.get(1).getName()))
+                .andExpect(jsonPath("$[1].role").value(players.get(1).getRole().toString()));
+    }
+
+    @Test
+    @DisplayName("Get Player Games - Should return 200 OK with player games")
+    public void getPlayerGames_ShouldReturnOkWithPlayerGames() throws Exception {
+        ObjectId playerId = new ObjectId("655c7adf06e4ae59f47979ca");
+        List<Game> games = this.games;
+
+        given(playerService.getPlayerGames(playerId)).willReturn(games);
+
+        // Act and Assert
+        mockMvc.perform(get("/players/{id}/games", playerId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(games.size()))
+                .andExpect(jsonPath("$[0].name").value("Game1"))
+                .andExpect(jsonPath("$[1].name").value("Game2"))
+                .andExpect(jsonPath("$[2].name").value("Game3"));
     }
 }
